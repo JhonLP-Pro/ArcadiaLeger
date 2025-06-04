8b069f99866da4b924472fdd9df77f8a6d18c56f
Jhon Lapada <jhon.lapada7@gmail.com>
2025-06-04 15:23:06 +0200

    reservation

--- controllers/affichage/afficherUtilisateur.php
+++ controllers/affichage/afficherUtilisateur.php
@@ -3,8 +3,8 @@
 
 require_once(__DIR__ . '/../../config.php');
 require_once(BASE_PATH . '/bdd/Database.php');
-require_once(BASE_PATH . '/models/utilisateur.php');
-$utilisateur = new utilisateur($bdd);
+require_once(BASE_PATH . '/models/Utilisateur.php');
+$utilisateur = new Utilisateur($bdd);
 
 
 $allUsers = $utilisateur->allUtilisateur();
